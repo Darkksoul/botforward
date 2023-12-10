d2a286ff245361d7c21ee133eb6adf02259c0641
Darkksoul <139141380+Darkksoul@users.noreply.github.com>
2023-12-10 16:24:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     API_ID = int(os.environ.get("API_ID", ""))
     API_HASH = os.environ.get("API_HASH", "")       
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    BOT_SESSION = os.environ.get("BOT_SESSION", "forwardbot")
+    BOT_SESSION = os.environ.get("BOT_SESSION", "")
     OWNER_ID = os.environ.get("OWNER_ID", "")                             
     DATABASE_URI = os.environ.get("DATABASE_URI", "")  
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluste0")
